774a06217c1ab6a04b328aba5a50bc00a789a582
Vicky Soni <murlimanoharsoni4@gmail.com>
2022-12-22 00:12:20 +0530

    added theme toggling using darkMode: 'class' of tailwindcss

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,10 @@
+//theme setting
+const root = document.documentElement; //root node
+const tglBtn = document.getElementById("toggle-theme");
+tglBtn.addEventListener("click", () => {
+    root.classList.toggle("dark");
+});
+////
 
 const qrImageContainer = document.getElementById("qr-image");
 const generated = document.getElementById("generated");
@@ -8,8 +15,8 @@ const selectBox = document.getElementById("qr-size");
 
 const downBtn = document.createElement("a");
 downBtn.innerText = "Download QR";
-downBtn.classList = "px-8 py-2 mt-10 max-w-[400px] rounded bg-red-400 text-yellow-200 cursor-pointer hover:text-green-600 hover:shadow-md hover:shadow-grey-200 block text-center mx-auto md:mb-10"
-
+downBtn.classList =
+    "px-8 py-2 mt-10 max-w-[400px] rounded bg-red-400 text-yellow-200 cursor-pointer hover:text-green-600 hover:shadow-md hover:shadow-grey-200 block text-center mx-auto md:mb-10 focus:outline focus:outline-3";
 
 const generateQr = (text, size) => {
     console.log(text);
@@ -21,20 +28,20 @@ const generateQr = (text, size) => {
         colorLight: "#ffffff",
         correctLevel: QRCode.CorrectLevel.H,
     });
-}
+};
 
 const clearGenerated = () => {
     qrImageContainer.innerHTML = "";
     downBtn.remove();
-}
+};
 
 const displayDownBtn = () => {
     const downLink = qrImageContainer.querySelector("img").src;
     console.log(downLink);
     downBtn.href = downLink;
-    downBtn.download = 'qrcode';
+    downBtn.download = "qrcode";
     generated.appendChild(downBtn);
-}
+};
 
 const handleGenQr = () => {
     //clear previous qr
@@ -42,11 +49,11 @@ const handleGenQr = () => {
     //gen new qr
     const inputText = inputBox.value;
     const qrSize = Number(selectBox.value);
-    if(inputText !== "" && qrSize !== isNaN(qrSize)) {
-       generateQr(inputText, qrSize);
+    if (inputText !== "" && qrSize !== isNaN(qrSize)) {
+        generateQr(inputText, qrSize);
     }
     //display download anchor
     setTimeout(displayDownBtn, 50); //waiting 50ms otherwise it doesnt exist in dom
-}
+};
 
 qrGenBtn.addEventListener("click", handleGenQr);
